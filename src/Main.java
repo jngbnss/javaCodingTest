79cca7de232170e4b1a68a0e50580b7c44a5af83
jngbnss <jngbnmn@naver.com>
2026-01-18 14:53:49 +0900

    save

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i][0]+" "+arr[i][1]);
-        }
+
+        System.out.println(arr[n - k]);
+
     }
 }
